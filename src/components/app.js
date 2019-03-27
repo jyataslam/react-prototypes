055407faac092a5f70ff1d246accbb106359ae5e
Jason <jasonyata@gmail.com>
2019-03-26 18:32:42 -0700

    functional component prototype complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,26 @@
+import React from 'react';
+import Table from './table';
 
+const students = [{
+        student: 'Peter Parker',
+        course: 'Intro to Photography',
+        grade: 95
+    }, {
+        student: 'Bruce Banner',
+        course: 'Psychology',
+        grade: 91
+    }, {
+        student: 'Tony Stark',
+        course: 'Mechanical Engineering',
+        grade: 100
+    }
+];
 
+const App = () => (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+);
+
+export default App;
