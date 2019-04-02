525c79707e278cc62a420d776d97b3832e1c7ae7
Jason <jasonyata@gmail.com>
2019-04-01 19:27:19 -0700

    Basic funcctunality complete Focus on hover complete Rest of extras not complete yet

---
+++ src/components/image_data.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import fatpug from './images/fat-pug.jpg';
+import pug2 from './images/pug-puppy.jpg';
+import pug3 from './images/pug.jpg';
+import pug4 from './images/pug4.jpg';
+import pug5 from './images/pug5.jpg';
+import pug6 from './images/pug6.jpg';
+import pug7 from './images/pug7.jpeg';
+import pug8 from './images/pug8.jpeg';
+
+export default [{
+    src: fatpug,
+    style: {
+        left: '5%',
+        transform: 'rotate(-10deg)'
+    }
+},
+{
+    src: pug2,
+    style: {
+        left: '40%',
+        transform: 'rotate(6deg)'
+    }
+},
+{
+    src: pug3,
+    style: {
+        left: '15%',
+        transform: 'rotate(-9deg)'
+    }
+},
+{
+    src: pug4,
+    style: {
+        left: '8%',
+        top: '48%',
+        transform: 'rotate(8deg)'
+    }
+},
+{
+    src: pug5,
+    style: {
+        left: '37%',
+        top: '25%',
+        transform: 'rotate(-12deg)'
+    }
+},
+{
+    src: pug6,
+    style: {
+        right: '12%',
+        top: '26%',
+        transform: 'rotate(10deg)'
+    }
+},
+{
+    src: pug7,
+    style: {
+        left: '25%',
+        top: '48%',
+        transform: 'rotate(-5deg)'
+    }
+},
+{
+    src: pug8,
+    style: {
+        right: '25%',
+        top: '52%',
+        transform: 'rotate(16deg)'
+    }
+},
+];
